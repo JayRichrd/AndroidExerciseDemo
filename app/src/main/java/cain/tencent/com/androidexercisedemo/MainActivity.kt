bcdb9960488ada60142935fac07e1ee0d763baf8
cainjiang <cainjiang@tencent.com>
2018-10-11 22:00:38 +0800

    添加线程分析问题

--- app/src/main/java/cain/tencent/com/androidexercisedemo/MainActivity.kt
+++ app/src/main/java/cain/tencent/com/androidexercisedemo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import cain.tencent.com.androidexercisedemo.databinding.ActivityMainBinding
@@ -20,6 +21,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun initView() {
         binding.btnFresco.setOnClickListener(this)
+        binding.btnThread.setOnClickListener(this)
     }
 
     override fun onClick(view: View?) {
@@ -27,9 +29,21 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             R.id.btn_fresco -> {
                 startActivity(Intent(this, FrescoActivity::class.java))
             }
+            R.id.btn_thread -> {
+                startThread()
+            }
             else -> {
 
             }
         }
+    }
+
+    private fun startThread() {
+        val thread = Thread(Runnable {
+            Log.i("Jay", "创建线程了.")
+            Thread.sleep(2 * 1000)
+        })
+        thread.name = "jay-thread"
+        thread.start()
     }
 }
