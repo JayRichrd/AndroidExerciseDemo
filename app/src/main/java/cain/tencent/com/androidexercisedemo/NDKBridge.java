a72ad5334e6e02d70ad50a77cd59ad02a5c05f95
cainjiang <cainjiang@tencent.com>
2019-03-12 10:09:07 +0800

    feat(lame): 添加lame源码编译库
    todo：转码出来的声音不对，偏慢
    参考博客：
    https://www.jianshu.com/p/fb531239cd79

--- app/src/main/java/cain/tencent/com/androidexercisedemo/NDKBridge.java
+++ app/src/main/java/cain/tencent/com/androidexercisedemo/NDKBridge.java
@@ -12,6 +12,12 @@ public class NDKBridge {
     // 获取lame版本号
     public native String getLameVersion();
 
-    // 将wav文件转换成mp3
+    /**
+     * 将wav文件转换成mp3
+     *
+     * @param wav          wav源文件
+     * @param map3         mp3目标文件
+     * @param inSamplerate 采样率
+     */
     public native void wav2Mp3(String wav, String map3, int inSamplerate);
 }
